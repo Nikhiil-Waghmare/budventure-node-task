80e1f4cdb923c72b6e352e3efb451452cbe0b301
Nikhil_Waghmare <nikhilw0969@gmail.com>
2026-06-05 19:33:03 +0530

    feat: initialize Prisma schema, update database connection settings, and reconfigure Postgres port mapping

---
+++ prisma/migrations/20260605140143_init/migration.sql
@@ -0,0 +1,36 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "wallet_balance" DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "items" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT "items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reservations" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "item_id" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'confirmed',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "reservations_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "reservations" ADD CONSTRAINT "reservations_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reservations" ADD CONSTRAINT "reservations_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "items"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
